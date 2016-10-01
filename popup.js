9dfac0f9a3ba2401af621826006bad5cb93c554e
jacquelineyoung8 <jyoung4@wellesley.edu>
2016-10-01 17:01:37 -0400

    wrote functions updatePhrase and backspace

---
+++ popup.js
@@ -0,0 +1,26 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var phrase = "";
+//   document.addEventListener("keypress", updatePhrase, false);
+// }, false);
+
+var phrase = "";
+
+
+function updatePhrase(e){
+  console.log("----------");
+  var key = e.key;
+  console.log("before: "+ phrase);
+  phrase += key;
+  console.log("after: "+phrase);
+};
+
+function backspace(e){
+  console.log("---------");
+  if (e.keyCode == 8 || e.keyCode == 46){
+    phrase = phrase.slice(0, phrase.length - 1);
+    console.log("phrase is: " + phrase);
+  }
+};
+
+document.addEventListener("keypress", updatePhrase, false);
+document.addEventListener("keydown", backspace, false);
